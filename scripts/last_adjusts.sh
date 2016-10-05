435ca1cf904714f580c71ad628e8afdd63177f2c
Carlos Edo <carlos.edo@ticketea.com>
2016-10-05 23:03:28 +0200

    added symlink for sublime cli

--- scripts/last_adjusts.sh
+++ scripts/last_adjusts.sh
@@ -4,5 +4,8 @@ set -eu
 set -o pipefail
 
 
+echo "Creating symlink for sublime"
+ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin/subl
+
 echo "Installing Xcode Command Line Tools"
 xcode-select --install
