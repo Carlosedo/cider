4e44e4275ce337a8a270099626d380eb1112010d
Carlos Edo <carlos.edo@ticketea.com>
2016-10-06 19:38:16 +0200

    Dont install command line tools, as they are needed in a previous step

--- scripts/last_adjusts.sh
+++ scripts/last_adjusts.sh
@@ -6,6 +6,3 @@ set -o pipefail
 
 echo "Creating symlink for sublime"
 ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin/subl
-
-echo "Installing Xcode Command Line Tools"
-xcode-select --install
