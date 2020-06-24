5ae40941e4bd6e96551c969ad87b3e0a5e914a83
Carlos Edo <carlosedo14@gmail.com>
2020-06-24 17:20:22 +0200

    more fixes

--- scripts/defaults.sh
+++ scripts/defaults.sh
@@ -19,8 +19,3 @@ chflags nohidden ~/Library
 
 echo "Allow applications from anywere"
 sudo spctl --master-disable
-
-
-# Kill affected applications
-for app in Finder Dock Mail Safari SystemUIServer; do killall "$app" > /dev/null 2>&1; done
-echo "Done. Note that some of these changes require a logout/restart to take effect."
